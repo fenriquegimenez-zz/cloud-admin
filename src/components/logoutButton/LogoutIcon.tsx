4cf25b3ec1f859ae1f518b91a2f13e4addb63d62
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-05-27 12:10:09 -0400

    tooltip fixed

--- src/components/logoutButton/LogoutIcon.tsx
+++ src/components/logoutButton/LogoutIcon.tsx
@@ -1,8 +1,13 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useAuth0 } from "@auth0/auth0-react"
+import ReactTooltip from "react-tooltip"
 
 const LogoutIcon = () => {
   const { logout } = useAuth0()
+
+  useEffect(() => {
+    ReactTooltip.rebuild()
+  }, [])
 
   return (
     <button
