71af2b0f1ff459b8292d8bf57f3ab37fbeb23378
Enrique Giménez <fenriquegimenez@gmail.com>
2021-07-07 17:25:00 -0400

    added environment variables to firestore keys

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNgxwc83Wpf9SU9eIPVypNyk1ggbEJpPU",
-  authDomain: "cloudadmin-298a3.firebaseapp.com",
-  projectId: "cloudadmin-298a3",
-  storageBucket: "cloudadmin-298a3.appspot.com",
-  messagingSenderId: "1002265695027",
-  appId: "1:1002265695027:web:f2e239477c891aabe3e446",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 }
 
 const fb = !firebase.apps.length
